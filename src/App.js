f758ea8de4179c42cd9c4f640a6a50df2c2d92ea
smdurjoy <smdurjoy963@gmail.com>
2020-08-12 12:36:34 +0600

    Initail Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [ input, setInput ] = useState('');
+  return (
+    <div className="App">
+      <h1>Welcome to dJes liveChat</h1>
+    </div>
+  );
+}
+
+export default App;
